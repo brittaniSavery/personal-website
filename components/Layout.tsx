f1da5e787419dc6711e75404e0ec396d695066eb
Brittani S Avery <brittani.s.avery@gmail.com>
2021-01-13 11:25:10 -0600

    Add v1 of analytics since v2 is still WIP (missing features)

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -34,7 +34,7 @@ export default function Layout({ meta, children }: LayoutProps): JSX.Element {
         <link rel="manifest" href="/site.webmanifest" />
         <script
           async
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_ANALYTICS_ID}`}
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_ANALYTICS_ID_V2}`}
         />
         <script src="/my-gtag.js" />
         <script src="https://code.iconify.design/1/1.0.7/iconify.min.js" />
--- public/my-gtag.js
+++ public/my-gtag.js
@@ -4,4 +4,5 @@ function gtag() {
 }
 gtag("js", new Date());
 
-gtag("config", "G-WQCF0JB188");
+gtag("config", process.env.NEXT_PUBLIC_ANALYTICS_ID_V1);
+gtag("config", process.env.NEXT_PUBLIC_ANALYTICS_ID_V2);
